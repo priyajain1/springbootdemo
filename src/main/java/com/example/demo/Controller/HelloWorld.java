bde7eeeba43ccf7e38920aa9e4770f399a622e88
priyajain1 <jain72priya@gmail.com>
2018-08-07 16:46:18 +0530

    test

---
+++ src/main/java/com/example/demo/Controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.demo.Controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorld {
+	@RequestMapping("/" )
+    String hello() {
+        return "Hello World!";
+    }
+}
